a5a06f4629191c51f5d07940148dae4921ded5ce
Tony <coolhzb@163.com>
2014-10-26 22:06:30 +0800

    BlockItem update…..

--- src/main/java/com/xqbase/block/BlockItem.java
+++ src/main/java/com/xqbase/block/BlockItem.java
@@ -3,8 +3,10 @@ package com.xqbase.block;
 public class BlockItem {
 
     private ItemMeta meta;
-
+    private byte[] key;
+    private byte[] value;
+    
     public BlockItem() {
     }
-    
+
 }
